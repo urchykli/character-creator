cfc69aa4ea86d889d78acb1802710d1449d2ca0c
Lindsay Allison <lindsayallison@macbook-pro.mynetworksettings.com>
2024-10-31 10:06:07 -0400

    Exercise 4 done

--- src/components/CharacterEditor/CharacterEditor.js
+++ src/components/CharacterEditor/CharacterEditor.js
@@ -30,12 +30,13 @@ function App() {
 
   return (
     <main className={styles.characterEditor}>
+      <div className={styles.characterBackground}></div>
       <MaxWidthWrapper className={styles.maxWidthWrapper}>
         <header className={styles.header}>
           <h1 className={styles.title}>Create your Character</h1>
           <p className={styles.description}>
-            Customize your character's look and style using the
-            controls below. What sort of adventure will you embark on?{' '}
+            Customize your character's look and style using the controls below.
+            What sort of adventure will you embark on?{" "}
           </p>
         </header>
         <div className={styles.controlColumn}>
